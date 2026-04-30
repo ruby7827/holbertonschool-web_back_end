6abb2044a40db05cf04295cc4c3c2223fef8d008
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-04-30 19:51:34 +0000

    add task 0

---
+++ 0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function that adds two floats.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of a and b as a float."""
+    return a + b
