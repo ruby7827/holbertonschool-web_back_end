16d6d66662f14409116c17f35add2232fd866707
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-05-01 04:51:48 +0000

    task 8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Complex types - functions
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies a float by multiplier.
+    """
+    def multiplier_func(n: float) -> float:
+        return n * multiplier
+
+    return multiplier_func
