11647d7d08dc82fc4bfd04f7cf4f064daf819f94
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-05-01 04:31:00 +0000

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for concat function."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and return the result."""
+    return str1 + str2
