0736ac9168ca9546a287cad0147104097ed7e751
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-07-21 19:57:46 +0000

    1. Simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+This module contains a Server class for paginating
+a dataset of popular baby names
+"""
+import csv
+from typing import List
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Returns the requested page of the dataset
+
+        Args:
+            page (int): the page number (must be > 0)
+            page_size (int): number of items per page (must be > 0)
+
+        Returns:
+            List[List]: the corresponding page of rows,
+            or an empty list if out of range
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+        data = self.dataset()
+
+        if start_index >= len(data):
+            return []
+
+        return data[start_index:end_index]
