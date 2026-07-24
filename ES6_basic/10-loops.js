24ea37ab6011ba9456c3b28cce0a31d97b5e71d0
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-07-24 17:25:08 +0000

    10. loops.js

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [idx, value] of array.entries()) {
-    // eslint-disable-next-line no-param-reassign
-    array[idx] = appendString + value;
-  }
+    const final = [];
+    for(const idx of array) {
+	final.push(appendString + idx);
+    }
 
-  return array;
+    return final;
 }
