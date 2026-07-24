d138305e98a7358cf0f46dc5084d02d04ac8daa5
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-07-24 10:40:42 +0000

    10. Fix eslint import/extensions error in main file

--- ES6_basic/10-main.js
+++ ES6_basic/10-main.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/extensions
 import appendToEachArrayValue from './10-loops.js';
 
 console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
