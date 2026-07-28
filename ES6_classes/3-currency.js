cb9b76e694238539ebcf9ce57b3ebe12a3165cae
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-07-28 17:41:44 +0000

    3. Methods, static methods, computed methods names..... MONEY

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
---
+++ ES6_classes/3-main.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/extensions
+import Currency from './3-currency.js';
+
+const dollar = new Currency('$', 'Dollars');
+console.log(dollar.displayFullCurrency());
