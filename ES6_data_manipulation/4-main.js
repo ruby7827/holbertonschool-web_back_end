9f1d260803377725284fdd50af241da78091114b
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-07-29 16:46:20 +0000

    4. Combine

---
+++ ES6_data_manipulation/4-main.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/extensions
+import getListStudents from './0-get_list_students.js';
+// eslint-disable-next-line import/extensions
+import updateStudentGradeByCity from './4-update_grade_by_city.js';
+
+console.log(updateStudentGradeByCity(getListStudents(), 'San Francisco', [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
+
+console.log(updateStudentGradeByCity(getListStudents(), 'San Francisco', [{ studentId: 5, grade: 97 }]));
