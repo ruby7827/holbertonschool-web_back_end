c2b03cc704c8a84c50174575e57f5b4a66ff6b5c
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-07-29 09:17:14 +0000

    6. Inheritance

---
+++ ES6_classes/6-main.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/extensions
+import SkyHighBuilding from './6-sky_high.js';
+
+const building = new SkyHighBuilding(140, 60);
+console.log(building.sqft);
+console.log(building.floors);
+console.log(building.evacuationWarningMessage());
---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-underscore-dangle */
+// eslint-disable-next-line import/extensions
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
