a0d31e844020d633bf579511c43ed5638ce27f69
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-07-29 09:42:42 +0000

    10. Vroom

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-underscore-dangle */
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const CarSpecies = this.constructor[Symbol.species];
+    return new CarSpecies();
+  }
+}
