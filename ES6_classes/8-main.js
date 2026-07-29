a73266e8fb32998b7a147fe44648861ec31925d8
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-07-29 09:28:34 +0000

    8. Primitive - Holberton Class

---
+++ ES6_classes/8-main.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/extensions
+import HolbertonClass from './8-hbtn_class.js';
+
+const hc = new HolbertonClass(12, 'Mezzanine');
+console.log(Number(hc));
+console.log(String(hc));
