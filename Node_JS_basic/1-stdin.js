1acea152eeb64ea80c5ea8b93cf9d789a214c621
ruby <rubgubadova7@gmail.com>
2026-08-08 15:10:35 +0000

    1. Simplify stdin handling

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,9 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false,
-});
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-rl.on('line', (line) => {
-  process.stdout.write(`Your name is: ${line}\n`);
-  rl.close();
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
 });
 
-rl.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
